7037c0eb72ae2a5ffac54cdd556eb1417824c000
Veeresh Wali <58882072+wali-veer@users.noreply.github.com>
2026-02-01 15:22:34 +0530

    correct indentation

--- main.tf
+++ main.tf
@@ -21,9 +21,9 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "web" {
-  ami = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.web.id]
+  ami                     = data.aws_ami.app_ami.id
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = [aws_security_group.web.id]
 
   tags = {
     Name = "terraform provisioned instance"
@@ -39,28 +39,28 @@ resource "aws_security_group "web" {
 }
 
 resource "aws_security_group_rule" "web_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  type                  = "ingress"
+  from_port             = 80
+  to_port               = 80
+  protocol              = "tcp"
+  cidr_blocks           = ["0.0.0.0/0"]
   vpc_security_group_id = aws_security_group.web.id
 }
 
 resource "aws_security_group_rule" "web_http_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  type                  = "ingress"
+  from_port             = 443
+  to_port               = 443
+  protocol              = "tcp"
+  cidr_blocks           = ["0.0.0.0/0"]
   vpc_security_group_id = aws_security_group.web.id
 }
 
 resource "aws_security_group_rule" "web.all_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  type                  = "egress"
+  from_port             = 0
+  to_port               = 0
+  protocol              = "-1"
+  cidr_blocks           = ["0.0.0.0/0"]
   vpc_security_group_id = aws_security_group.web.id
 }
