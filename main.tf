d002a424a0b4e58e4e083bebb2840f22bd88aa69
Veeresh Wali <58882072+wali-veer@users.noreply.github.com>
2026-02-01 17:27:17 +0530

    main file checkin

--- main.tf
+++ main.tf
@@ -1,66 +1,42 @@
 data "aws_ami" "app_ami" {
-  
+
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    name    = "name"
+    values  = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name    = "virtualization-type"
+    values  = ["hvm"]
   }
 
-  owners = ["123456789012]
-
+  owners = ["123412341234"]
 }
 
 data "aws_vpc" "default" {
   default = true
 }
 
+
 resource "aws_instance" "web" {
-  ami                     = data.aws_ami.app_ami.id
+  ami                     = data.aws_ami.app_ami.id 
   instance_type           = var.instance_type
-  vpc_security_group_ids  = [aws_security_group.web.id]
-
-  tags = {
-    Name = "terraform provisioned instance"
-  }
+  vpc_security_group_ids  = []
 }
 
-resource "aws_security_group "web" {
-  name = "web"
-  tags = {
-    Terraform = "true"
-  }
-  vpc_id = data.aws_vpc.default.id
-}
 
-resource "aws_security_group_rule" "web_http_in" {
-  type                  = "ingress"
-  from_port             = 80
-  to_port               = 80
-  protocol              = "tcp"
-  cidr_blocks           = ["0.0.0.0/0"]
-  vpc_security_group_id = aws_security_group.web.id
-}
+module "web_sg" {
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "4.13.0"
 
-resource "aws_security_group_rule" "web_http_in" {
-  type                  = "ingress"
-  from_port             = 443
-  to_port               = 443
-  protocol              = "tcp"
-  cidr_blocks           = ["0.0.0.0/0"]
-  vpc_security_group_id = aws_security_group.web.id
-}
+  vpc_id              = data.aws_vpc.default.id
+  name                = "web"
 
-resource "aws_security_group_rule" "web.all_out" {
-  type                  = "egress"
-  from_port             = 0
-  to_port               = 0
-  protocol              = "-1"
-  cidr_blocks           = ["0.0.0.0/0"]
-  vpc_security_group_id = aws_security_group.web.id
+  ingress_rule        = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  
+  egress_rules        = ["all_all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
