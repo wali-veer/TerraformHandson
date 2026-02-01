67a165de9b7f85a587a6f2bcdbccc70f90a24fed
Veeresh Wali <58882072+wali-veer@users.noreply.github.com>
2026-02-01 17:30:10 +0530

    modified main file

--- main.tf
+++ main.tf
@@ -21,9 +21,15 @@ data "aws_vpc" "default" {
 
 
 resource "aws_instance" "web" {
+
   ami                     = data.aws_ami.app_ami.id 
   instance_type           = var.instance_type
-  vpc_security_group_ids  = []
+  vpc_security_group_ids  = [module.web_sg.vpc_security_group_id]
+
+  tags = {
+    name  = "Instance creted through module"
+  }
+
 }
 
 
