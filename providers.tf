39f6b6af64e9fcb2604c5694b9e4a99d6b02f5f0
Veeresh Wali <58882072+wali-veer@users.noreply.github.com>
2026-02-02 10:46:42 +0530

    first commit to refer variables in var file

--- providers.tf
+++ providers.tf
@@ -6,6 +6,7 @@ terraform {
     }
 }
 
+
 provider "aws" {
     region = "us-west-2"
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,46 @@
 variable "instance_type" {
-  description = "Type of EC2 instabce we provision"
-  default = "t2.nano"
+  description = "Type of the EC2 instance we want to provision"
+  default     = "t3.nano"
+}
+
+variable "ami_filter" {
+  description = "Filter for AMI and its owner"
+
+  type = object (
+    {
+      name  = string
+      owner = string
+    }
+  )
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "123098456098" 
+  }
+}
+
+variable "environment" {
+  description = "Name of the environment to be created"
+
+  type object (
+    {
+      name = string
+      network_prefix = string      
+    }
+  )
+
+  default = {
+    name            = "dev"
+    network_prefix  = "10.0"
+  }
+}
+
+variable "asg_min" {
+  description = "Minimum number of instances to be provisoned"
+  default = 1
+}
+
+variable "asg_max" {
+  description = "maximum number of instances to be provisoned"
+  default = 5
 }
